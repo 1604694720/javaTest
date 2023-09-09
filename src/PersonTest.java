e038f1e9fba21ec7db8f96ceb2a4c7a90393e46d
YangShi <1604694720@qq.com>
2023-09-09 21:32:26 +0800

    类的创建与调用练习1

--- src/PersonTest.java
+++ src/PersonTest.java
@@ -9,6 +9,9 @@
 1.创建类，设计类的对象
 2.创建类的对象
 3.调用对象
+
+三、如果创建了一个类的多个对象，这每个对象都独立的拥有一套类的属性。（非static）
+   如果我们修改了一个对象的属性a的值，另一个对象不受影响。
  */
 public class PersonTest {
     public static void main(String[] args) {
@@ -24,6 +27,21 @@ public class PersonTest {
         p1.eat();
         p1.sleep();
         p1.talk("English");
+        //******************************************************
+        Person p2=new Person();
+        System.out.println(p2.name);//Tom? null? 报错？
+        System.out.println(p2.isMale);//null。默认初始化值。
+        //****************************************
+        //p1变量的地址值给了p3，导致p1和p3都指向了对空间中同一个对象实体
+        Person p3=p1;
+        System.out.println(p3.name);//Tom
+
+        p3.age=10;
+        System.out.println(p1.age);//10
+
+        Person p4=new Person();
+        p4=p1;
+        System.out.println(p4.name);
     }
 }
 class Person{
